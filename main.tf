ecd092bf907444626be3d12c38920f658e78fc55
Masato-Nishioka <nishioka.masa0124@gmail.com>
2025-01-08 20:10:37 +0900

    refactor: 必要最低限のステータス設定にリファクタリング

--- main.tf
+++ main.tf
@@ -1,36 +1,27 @@
-# Identity pool
-# (https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/iam_workload_identity_pool_provider#example-usage---iam-workload-identity-pool-provider-github-actions)
-
 resource "google_iam_workload_identity_pool" "pool" {
   project                   = var.project_id
   workload_identity_pool_id = var.pool_id
-  display_name              = var.pool_display_name
-  description               = var.pool_description
-  disabled                  = var.pool_disabled
 }
-
-# Identity pool provider
 
 resource "google_iam_workload_identity_pool_provider" "provider" {
   project                            = var.project_id
   workload_identity_pool_id          = var.pool_id
   workload_identity_pool_provider_id = var.provider_id
-  display_name                       = var.provider_display_name
-  description                        = var.provider_description
-  disabled                           = var.provider_disabled
   attribute_mapping                  = var.attribute_mapping
   attribute_condition                = "assertion.repository == '${var.repository_owner_or_organization}/${var.repository_name}'"
   oidc {
     issuer_uri = var.issuer_uri
   }
+  depends_on = [
+    google_iam_workload_identity_pool.pool
+  ]
 }
-
-# Service account impersonation
-# (https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account_iam#google_service_account_iam_member)
-
 
 resource "google_service_account_iam_member" "admin-account-iam" {
   service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_email}"
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.pool_id}/attribute.repository/${var.repository_owner_or_organization}/${var.repository_name}"
+  depends_on = [
+    google_iam_workload_identity_pool.pool
+  ]
 }
